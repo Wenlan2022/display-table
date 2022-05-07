fd336b19c975abefb6833d90e470cf7ca1097d35
Wenlan Tan <wenlan.tan1@gmail.com>
2022-05-06 22:03:03 -0400

    create sort function

--- src/component/Display.jsx
+++ src/component/Display.jsx
@@ -1,44 +1,46 @@
-import React,{useState} from 'react'
+import React from 'react'
 
 export default function Display(props) {
     
-    // console.log(props.input)
-
-    const [data, setData] = useState([...props.input])
-    // const [sort, setSort] = useState([])
-    
-  
-        // console.log("------------------------")
-
-
-    console.log(data)
-
-    
+    let tmpData = []
+    tmpData = [...props.input]
 
     function sortByFirstName(){
-        console.log("hello")
-        let tmpData = []
-        tmpData = [...data]
-        
+  
         tmpData.sort((a,b)=>( a.firstname > b.firstname? 1:-1))
-        setData(tmpData)
+        props.setData(tmpData)
 
+    }
+
+    function sortByLastName(){
+        
+        tmpData.sort((a,b)=>( a.lastname > b.lastname? 1:-1))
+        props.setData(tmpData)
+    }
+
+    function sortByAgee(){
+        
+        tmpData.sort((a,b)=>( a.age > b.age? 1:-1))
+        props.setData(tmpData)
     }
   return (
     <div>
         <table>
+            <thead>
         <tr>
             <th onClick={sortByFirstName}>First Name</th>
-            <th>Last Name</th>
-            <th>Age</th>
+            <th onClick={sortByLastName}>Last Name</th>
+            <th onClick={sortByAgee}>Age</th>
         </tr>
-        {data.length > 0 ?data.map((item,index) =>(
+        </thead>
+        <tbody>
+        {props.input.map((item,index) =>(
         <tr key = {index}>
             <td>{item.firstname}</td>
             <td>{item.lastname}</td>
             <td>{item.age}</td>
-        </tr>
-    )):"loading"}
+        </tr>))}
+    </tbody>
         </table>
     </div>
   )
